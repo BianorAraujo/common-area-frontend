bf73bc4642ad1e378aee056141d5f060b7f62f74
Bianor Araujo <bianor.araujo@gmail.com>
2025-04-18 22:22:37 +0100

    Fix Login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from "./axiosConfig";
+import axios from "./../axiosConfig";
 
 export const AuthContext = createContext();
 
